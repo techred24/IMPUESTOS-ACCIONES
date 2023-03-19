0d5d7053749042b04a226cd750ec22282cc67622
TeChRF24 <f.rafael.temich24@gmail.com>
2023-03-19 04:43:37 -0600

    Showing selected indexed

---
+++ helpers.js
@@ -0,0 +1,75 @@
+export const createElement = ({ name, attributes }) => {
+    const element = document.createElement(name);
+    for (const [attribute, value] of Object.entries(attributes)) {
+        element.setAttribute(attribute, value);
+    }
+    return element;
+}
+
+export const removeAttributeById = (id) => document.getElementById(id).remove();
+
+export const request = async (dateData, anios) => {
+    const resp = await fetch('https://www.banxico.org.mx/SieAPIRest/service/v1/series/SP1/datos?' + new URLSearchParams({
+        'token': 'ea526ff2d8d0cce16cd66f8aa4fb078b66d1e6ce0b3b6b8c9c8dcbb25e65533e',
+    }));
+        const dias = (await resp.json())?.bmx?.series[0]?.datos ?? [];
+        if (!dias) return
+        let datosReversa = dias.reverse();
+            for (let dia of datosReversa) {
+                // console.log(dia, 'DIA')
+                if (dateData.includes(dia.fecha.slice(3))) return
+                dateData.push({fecha: dia.fecha.slice(3), dato: dia.dato});
+                if (anios.includes(dia.fecha.slice(6))) continue
+                anios.push(dia.fecha.slice(6));
+            }
+    }
+ export const changeDates = (yearToFind, [...dateData]) => {
+        const $inpcContainer = document.getElementById('inpc__indices');
+        const inpcData = $inpcContainer.children;
+        deletePreviousElements(inpcData);
+        let newDates = dateData.filter(date => date.fecha.includes(yearToFind));
+        createElementsForMonthData(newDates);
+    }
+    const deletePreviousElements = (prevElements) => {
+        if (prevElements.length === 0) return
+        [...prevElements].forEach(element => element.remove());
+    }
+export const createElementsForMonthData = (newDates) => {
+    const elementDataContainer = {
+        name: 'div',
+        attributes: {
+            class: 'inpc__indice'
+        }
+    }
+    const dateElement = {
+        name: 'p',
+        attributes: {
+            class: 'inpc__fecha',
+            style: 'pointer-events: none'
+        }
+    }
+    const dataElement = {
+        name: 'p',
+        attributes: {
+            class: 'inpc__dato',
+            style: 'pointer-events: none'
+        }
+    }
+    for (const infoToAdd of newDates) {
+        const elementToAdd = createElement(elementDataContainer);
+        const dato = createElement(dataElement);
+        dato.innerText = infoToAdd.dato;
+        const fecha = createElement(dateElement);
+        fecha.innerText = infoToAdd.fecha;
+        elementToAdd.appendChild(fecha);
+        elementToAdd.appendChild(dato);
+        document.getElementById('inpc__indices').prepend(elementToAdd);
+    }
+}
+
+export const setIndicesForUser = (id, { fecha, dato }) => {
+    const indexToShow = document.getElementById(id);
+    console.log(indexToShow, 'TAG SELECTED')
+    indexToShow.innerText = '';
+    indexToShow.innerText = `${dato} (${fecha})`;
+}
--- main.js
+++ main.js
@@ -1,48 +1,23 @@
-const fechas = [];
+import { request, createElement, changeDates, setIndicesForUser } from "./helpers.js";
+const anios = [];
 const dateData = [];
 let kindOfIndexToAdd = '';
-async function request() {
-    const resp = await fetch('https://www.banxico.org.mx/SieAPIRest/service/v1/series/SP1/datos?' + new URLSearchParams({
-        'token': 'ea526ff2d8d0cce16cd66f8aa4fb078b66d1e6ce0b3b6b8c9c8dcbb25e65533e',
-    }),
-    // {
-    //     headers: {
-    //     'Access-Control-Allow-Origin': '*',
-    //     'Access-Control-Allow-Headers': 'OPTIONS, GET, POST, UPDATE, DELETE',
-    //     'Access-Control-Allow-Headers': 'authorization',
-    // }
-    // }
-    );
-        const datos = await resp.json();
-        // console.log(datos);
-        // console.log(datos.bmx.series[0].datos)
-        const dias = datos?.bmx?.series[0]?.datos;
-        if (!dias) return
-        let datosReversa = dias.reverse();
-        // console.log(datosReversa, 'Reversa');
-            for (let dia of datosReversa) {
-                // console.log(dia, 'INDIVIDUAL')
 
-                if (dateData.includes(dia.fecha.slice(3))) return
-                dateData.push({fecha: dia.fecha.slice(3), dato: dia.dato});
-                if (fechas.includes(dia.fecha.slice(6))) continue
-                fechas.push(dia.fecha.slice(6));
-            }
-            // assignFirtYear();
-    }
-request();
+// if (indexedDB) {
 
+// } else {
+    request(dateData, anios);
+// }
 let $currentSelectedYear = document.getElementById('currentSelectedYear');
 const $openModalButtonPurchase = document.getElementById('selectIndexPurchase');
 const $openModalButtonSell = document.getElementById('selectIndexSell');
 const $closeModalButton = document.getElementById('modal__button-close');
 const $modal = document.getElementById('modal');
 
-
 const assignFirtYear = () => {
-    let firstYear = fechas[0];
+    let firstYear = anios[0];
     $currentSelectedYear.innerText = firstYear;
-    changeDates(Number(firstYear));
+    changeDates(Number(firstYear), dateData);
 }
 $openModalButtonPurchase.addEventListener('click', () => {
     $modal.classList.add('target');
@@ -57,35 +32,31 @@ $openModalButtonSell.addEventListener('click', () => {
 $closeModalButton.addEventListener('click', () => {
     $modal.classList.remove('target');
 });
-const changeDates = (yearToFind) => {
-    const $inpcContainer = document.getElementById('inpc__indices');
-    const inpcData = $inpcContainer.children;
-    let newDates = [];
-    for (let data of dateData) {
-        if (newDates.length >= 12) break;
-        if (data.fecha.includes(yearToFind)) newDates.push(data);
-    }
-    newDates = newDates.reverse();
-    for (let i in [...inpcData]) {
-        inpcData[i].children[0].innerText = newDates[i]?.fecha ?? '';
-        inpcData[i].children[1].innerText = newDates[i]?.dato ?? '';
-    }
-}
 
 document.addEventListener('DOMContentLoaded', () => {
     const $backArrow = document.getElementById('back_arrow');
     const $forwardArrow = document.getElementById('forward_arrow');
     $backArrow.addEventListener('click', () => {
-        console.log('click back');
         let previousYear = Number($currentSelectedYear.innerText) - 1;
-        if (!fechas.includes(previousYear.toString())) return;
+        if (!anios.includes(previousYear.toString())) return;
             $currentSelectedYear.innerText = previousYear;
-            changeDates(previousYear);
+            changeDates(previousYear, dateData);
     });
     $forwardArrow.addEventListener('click', () => {
         let nextYear = Number($currentSelectedYear.innerText) + 1;
-        if (!fechas.includes(nextYear.toString())) return;
+        if (!anios.includes(nextYear.toString())) return;
             $currentSelectedYear.innerText = nextYear;
-            changeDates(nextYear);
+            changeDates(nextYear, dateData);
     });
+});
+
+document.getElementById('inpc__indices').addEventListener('click', (event) => {
+    const closeModalButton = document.getElementById('modal__button-close');
+    if (event.target.className === 'inpc__indice') {
+        console.log(event.target.children[0].innerText, event.target.children[1].innerText);
+        const id = kindOfIndexToAdd === 'purchase' ? 'purchase-figure' : 'sell-figure';
+        console.log(id, 'ID');
+        setIndicesForUser(id, { fecha: event.target.children[0].innerText, dato: event.target.children[1].innerText });
+        closeModalButton.click();
+    }
 });
