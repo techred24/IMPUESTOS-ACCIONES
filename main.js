212708fadee6c1ec4bc7974794398a2934cec043
TeChRF24 <f.rafael.temich24@gmail.com>
2023-08-03 21:53:46 -0600

    Fixing arrow position in date tabs

--- main.js
+++ main.js
@@ -6,64 +6,6 @@ const dateData = [];
 
     request(dateData, anios);
 
-
-// let $currentSelectedYear = document.getElementById('currentSelectedYear');
-// const $openModalButtonPurchase = document.getElementById('selectIndexPurchase');
-// const $openModalButtonSell = document.getElementById('selectIndexSell');
-// const $closeModalButton = document.getElementById('modal__button-close');
-// const $modal = document.getElementById('modal');
-// const $inpcContainer = document.getElementById('inpc__indices');
-
-// const getMonthsForCurrentYear = () => {
-//     let firstYear = anios[0];
-//     $currentSelectedYear.innerText = firstYear;
-//     const monthsForYear = findMonthsForYear(Number(firstYear), dateData);
-//     return monthsForYear;
-// }
-// $openModalButtonPurchase.addEventListener('click', () => {
-//     const adapter = new PurchaseModalAdapter(getMonthsForCurrentYear());
-//     ListView.showModal(adapter);
-// });
-// $openModalButtonSell.addEventListener('click', () => {
-//     const adapter = new SellModalAdapter(getMonthsForCurrentYear());
-//     ListView.showModal(adapter);
-// });
-// $closeModalButton.addEventListener('click', () => {
-//     $modal.classList.remove('target');
-// });
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     const $backArrow = document.getElementById('back_arrow');
-//     const $forwardArrow = document.getElementById('forward_arrow');
-//     $backArrow.addEventListener('click', () => {
-//         let previousYear = Number($currentSelectedYear.innerText) - 1;
-//         if (!anios.includes(previousYear.toString())) return;
-//             $currentSelectedYear.innerText = previousYear;
-//             findMonthsForYear(previousYear, dateData);
-//     });
-//     $forwardArrow.addEventListener('click', () => {
-//         let nextYear = Number($currentSelectedYear.innerText) + 1;
-//         if (!anios.includes(nextYear.toString())) return;
-//             $currentSelectedYear.innerText = nextYear;
-//             findMonthsForYear(nextYear, dateData);
-//     });
-// });
-
-// document.getElementById('inpc__indices').addEventListener('click', (event) => {
-//     if (event.target.className === 'inpc__indice') {
-//         console.log(event.target.children[0].innerText, event.target.children[1].innerText);
-//         const id = $inpcContainer.dataset.type === 'purchase' ? 'purchase-figure' : 'sell-figure';
-//         setIndicesForUser(id, { fecha: event.target.children[0].innerText, dato: event.target.children[1].innerText });
-//         $closeModalButton.click();
-//     }
-// });
-
-// document.getElementById('sell-purchase__container').addEventListener('keypress', (event) => {
-//     event.preventDefault();
-//       if (event.target.value.includes('.') && event.key === '.') return
-//       let key = window.event ? event.which : event.keyCode
-//       if ((key >= 48 && key <= 57) || key === 46) event.target.value += event.key
-// });
 const $inpcContainer = document.getElementById('inpc__indices');
 const $closeModalButton = document.getElementById('modal__button-close');
 const clickListenerOnTab = (event) => {
@@ -270,12 +212,7 @@ tabsBox.addEventListener("mousedown", () => isDragging = true);
 tabsBox.addEventListener("mousemove", dragging);
 document.addEventListener("mouseup", dragStop);
 
-
-
-
-
-
-
+// IMPLEMENTING TOAST
 const notifications = document.querySelector(".notifications");
 
 const toastDetails = {
